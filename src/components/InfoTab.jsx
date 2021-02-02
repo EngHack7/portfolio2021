1ff7d6486fd34563d46acc1abce3ac36b046d978
EngHack7 <hamza.air267@gmail.com>
2021-02-03 01:19:09 +0200

    add work table

--- src/components/InfoTab.jsx
+++ src/components/InfoTab.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import './infotable.css'
 function InfoTab() {
   return (
     <>
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,31 +1,63 @@
 import React from "react";
+const we = [
+    {pn :'Chat-Rooms',tu:"React, nodeJs express, socketIO,",pu:"https://github.com/EngHack7/Chat-Rooms",pv:"https://first-chat-room.netlify.app/" },
+    {pn :'control-flow-graph automated tools',tu:"c# windows Application ,lexical Analyzer,  ",pu:"https://github.com/EngHack7/graduate-project-CFG",pv:"not deployed " },
+    {pn:"library-system",tu:"Angular, NodeJs, mySQL",pu : "https://github.com/EngHack7/library-Angular-NodeJS-",pv:"not deployed "},
+    {pn : "imstagram",tu : "React, NodeJs Express framework, MongoDb",pu:"soon ...",pv : "soon ..."}
+]
 
 function Works() {
   const style = {
     width: "max-content",
+    maxWidth  :"100%",
     margin: "auto",
     marginTop: "24px",
     fontSize: "38px",
   };
+  const td_style = {
+      display : 'table-cell'
+  }
   return (
     <React.Fragment>
-      <div style={style}>Works</div>
-      <table className="table table-striped table-hover">
+      <div style={style}> Best of some Works i have</div>
+      <div className="table-responsive">
+      <table className="table">
         <thead>
           <tr>
-            <th>project </th>
-            <th>github </th>
-            <th>view </th>
+            <th scope="col">project </th>
+            <th scope="col">technology used </th>
+            <th scope="col">github </th>
+            <th scope="col">view </th>
           </tr>
         </thead>
+      
         <tbody>
-          <tr>
-            <td>name</td>
-            <td>url src</td>
-            <td>views</td>
-          </tr>
+        {/* <tr scope="row">
+        <td>jjjjjjjjjjjjjjjjjjjjjjjjjjjjj</td>
+                        <td>jjjjjjjjjjjjjjjjjjjjjjjjjjjj</td>
+                        <td>
+                        <a href="" >lllllllllllllllllllll</a>
+                     
+                        </td>
+                        <td><a href="{e.pu}">llllllllllllllllllllllllll</a></td>
+        </tr> */}
+            {
+                we.map(e => (
+                    <tr scope="row" key={e}>
+                        <td style ={td_style}>{e.pn}</td>
+                        <td style ={td_style}>{e.tu}</td>
+                        <td style ={td_style}>
+                        <a href={e.pu} >{e.pu}</a>
+                     
+                        </td>
+                        <td style ={td_style}><a href={e.pu}>{e.pv}</a></td>
+                    </tr>
+                ))
+            }
+        
         </tbody>
       </table>
+      </div>
     </React.Fragment>
   );
 }
